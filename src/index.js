f504ae48ff6d078c6160660397a180b8f71d36eb
Juliana <juliana.jeanmarie972@gmail.com>
2018-11-02 17:17:00 +0100

    refacto

--- src/index.js
+++ src/index.js
@@ -4,6 +4,16 @@ var rollDice = require("./utils/roll-a-die");
 var boardArray = require("./utils/board-game");
 var MonopolyView = require("./view/monopoly");
 
+var game = new MonopolyView(monopoly);
+game.cellsEventListener(cellActionDispatcher);
+game.startGameBtn.addEventListener(
+  "click",
+  function() {
+    game.startGame(game);
+  },
+  false
+);
+
 function monopoly() {
   var resDice = rollDice(game.diceDisplay);
   var currentPlayer = game.players.find(function(player) {
@@ -19,14 +29,51 @@ function move(player, resDice) {
   } else {
     player.pawn.currentCellId = nextPos - game.MAX_CELL;
   }
+  console.log("**********************");
+  console.log("c'est au tour de ", player.name, " de jouer");
+  console.log(player.name, "lance les dés et obtient ", resDice);
+
+  updateDisplayWithMove(player);
+}
+
+function cellActionDispatcher(e) {
+  var currentPlayer = game.players.find(function(player) {
+    return player.current;
+  });
+  //on focus other cells without roll dice do nothing
+  if (parseInt(e.target.id) !== currentPlayer.pawn.currentCellId) {
+    return;
+  }
+  console.log("joueur courant =  ", currentPlayer.name);
+  var pos = currentPlayer.pawn.currentCellId;
+  var currentCell = boardArray[pos];
+  var isFreeToBuy = currentCell.isSellable();
+  var titleToBuy = game.bank.titleList.find(function(title) {
+    return title.cellId === currentCell.cellId;
+  });
+
+  if (isFreeToBuy) {
+    game
+      .checkPlayerResponse(titleToBuy, currentPlayer)
+      .then(function(responsePlayer) {
+        if (responsePlayer) {
+          doBuy(currentPlayer, currentCell, titleToBuy);
+        }
+      });
+    updatePlayers(currentPlayer);
+  } else {
+    if (currentCell.playerOwner) {
+      doPay(currentPlayer, currentCell, titleToBuy);
+    }
+    updatePlayers(currentPlayer);
+  }
+}
+
+function updateDisplayWithMove(player) {
   //wait before move pawn
   setTimeout(function() {
     game.movePawn(player.pawn);
   }, 2000);
-  console.log("**********************");
-
-  console.log("c'est au tour de ", player.name, " de jouer");
-  console.log(player.name, "lance les dés et obtient ", resDice);
 }
 
 function updatePlayers(player) {
@@ -43,73 +90,42 @@ function updatePlayers(player) {
   game.players = newPlayers;
 }
 
-function cellActionDispatcher(e) {
-  var currentPlayer = game.players.find(function(player) {
-    return player.current;
-  });
-
-  if (parseInt(e.target.id) !== currentPlayer.pawn.currentCellId) {
-    return;
-  }
-  console.log("joueur courant =  ", currentPlayer.name);
-
-  var pos = currentPlayer.pawn.currentCellId;
-  var currentCell = boardArray[pos];
-  var isFreeToBuy = currentCell.isSellable();
-  var titleToBuy = game.bank.titleList.find(function(title) {
-    return title.cellId === currentCell.cellId;
-  });
-
-  if (isFreeToBuy) {
-    game
-      .checkPlayerResponse(titleToBuy, currentPlayer)
-      .then(function(responsePlayer) {
-        if (responsePlayer) {
-          currentPlayer = game.bank.sellTitle(currentPlayer, titleToBuy);
-          if (
-            currentPlayer.titleList.find(function(title) {
-              title.id === titleToBuy.id;
-            }) !== null
-          ) {
-            currentCell.setPlayerOwner(currentPlayer);
-            console.log(currentPlayer.name, "achete ", titleToBuy.name);
-          } else {
-            console.log(
-              currentPlayer.name,
-              " n'a pas assez de capital pour ",
-              currentCell.name
-            );
-          }
-        }
-      });
-    updatePlayers(currentPlayer);
+function doBuy(currentPlayer, currentCell, titleToBuy) {
+  currentPlayer = game.bank.sellTitle(currentPlayer, titleToBuy);
+  if (
+    currentPlayer.titleList.find(function(title) {
+      title.id === titleToBuy.id;
+    }) !== null
+  ) {
+    currentCell.setPlayerOwner(currentPlayer);
+    console.log(currentPlayer.name, "achete ", titleToBuy.name);
   } else {
-    if (currentCell.playerOwner) {
-      if (currentCell.playerOwner.id === currentPlayer.id) {
-        console.log(currentPlayer.name, " est chez lui");
-        return;
-      }
-      var playerToPay = game.players.find(function(player) {
-        return player.current === false;
-      });
-      console.log(
-        currentPlayer.name,
-        "paye ",
-        titleToBuy.rent,
-        " à ",
-        playerToPay.name
-      );
-      currentPlayer.payRent(playerToPay, titleToBuy.rent);
-    }
-
-    updatePlayers(currentPlayer);
+    console.log(
+      currentPlayer.name,
+      " n'a pas assez de capital pour ",
+      currentCell.name
+    );
   }
 }
 
-var game = new MonopolyView(monopoly);
-game.cellsEventListener(cellActionDispatcher);
-var start = function() {
-  game.startGame(game);
-};
-game.startGameBtn.addEventListener("click", start, false);
+function doPay(currentPlayer, currentCell, titleToBuy) {
+  //at home do nothing
+  if (currentCell.playerOwner.id === currentPlayer.id) {
+    console.log(currentPlayer.name, " est chez lui");
+    return;
+  }
+
+  var playerToPay = game.players.find(function(player) {
+    return player.current === false;
+  });
+  console.log(
+    currentPlayer.name,
+    "paye ",
+    titleToBuy.rent,
+    " à ",
+    playerToPay.name
+  );
+  currentPlayer.payRent(playerToPay, titleToBuy.rent);
+}
+
 module.exports = monopoly;
