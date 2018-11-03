c799ef135e0d2ef2730a88cac93686f653e49b2c
Juliana <juliana.jeanmarie972@gmail.com>
2018-11-03 20:06:22 +0100

    fix price title

--- src/utils/titles.js
+++ src/utils/titles.js
@@ -6,53 +6,48 @@ var palaisJustice1885Title = new Title(
   5,
   "palais de justice 1885",
   "white",
-  20000
+  2500
 );
 var bureauMarshalTitle = new Title(6, "bureau du marshal", "blue", 600);
 var marechalFerrantTitle = new Title(8, "marechal-ferrant", "blue", 800);
-var palaceSaloonTitle = new Title(9, "palace saloon", "blue", 12000);
-var roysTitle = new Title(11, "roy's records", "pink", 14000);
-var texacoTitle = new Title(12, "texaco", "pink", 16000);
-var plutoniumTitle = new Title(13, "plutonium industries", "white", 15000);
-var cineEssexTitle = new Title(14, "cinema essex", "pink", 14000);
-var palaisJustice1955 = new Title(15, "palais de justice 1955", "white", 20000);
-var twinPinesTitle = new Title(16, "twin pines ranch", "orange", 18000);
-var louCafeTitle = new Title(18, "lou's cafe", "orange", 18000);
-var manoirBrownTitle = new Title(19, "le manoir des brown", "orange", 20000);
-var toyotaTitle = new Title(21, "statler toyota", "red", 22000);
+var palaceSaloonTitle = new Title(9, "palace saloon", "blue", 800);
+var roysTitle = new Title(11, "roy's records", "pink", 1000);
+var texacoTitle = new Title(12, "texaco", "pink", 1200);
+var plutoniumTitle = new Title(13, "plutonium industries", "white", 400);
+var cineEssexTitle = new Title(14, "cinema essex", "pink", 1000);
+var palaisJustice1955 = new Title(15, "palais de justice 1955", "white", 2500);
+var twinPinesTitle = new Title(16, "twin pines ranch", "orange", 1400);
+var louCafeTitle = new Title(18, "lou's cafe", "orange", 1400);
+var manoirBrownTitle = new Title(19, "le manoir des brown", "orange", 1600);
+var toyotaTitle = new Title(21, "statler toyota", "red", 1800);
 var louAerobicTitle = new Title(
   23,
   "lou's aerobic fitness center",
   "red",
-  22000
+  1800
 );
-var texacoRedTitle = new Title(24, "texaco", "red", 24000);
+var texacoRedTitle = new Title(24, "texaco", "red", 2000);
 var palaisJustice1985Title = new Title(
   25,
   "palais de justice 1985",
   "white",
-  20000
+  2500
 );
 var hillValleySchoolTitle = new Title(
   26,
   "hill valley high school",
   "yellow",
-  26000
+  2200
 );
-var twinPinesMallTitle = new Title(27, "twin pines mall", "yellow", 26000);
-var fusionTitle = new Title(28, "fusion indistries", "white", 15000);
-var lyonEstatesTitle = new Title(29, "lyon estates", "yellow", 28000);
-var pizzaHutTitle = new Title(31, "pizza hut", "green", 30000);
-var holomaxTitle = new Title(32, "holomax", "green", 30000);
-var hillDaleTitle = new Title(34, "hill dale", "green", 32000);
-var palaisJustice2015 = new Title(35, "palais de justice 2015", "white", 20000);
-var blastFromPastTitle = new Title(
-  37,
-  "blast from the past",
-  "darkblue",
-  35000
-);
-var cafe80Title = new Title(39, "cafe 80's", "darkblue", 40000);
+var twinPinesMallTitle = new Title(27, "twin pines mall", "yellow", 2200);
+var fusionTitle = new Title(28, "fusion industries", "white", 400);
+var lyonEstatesTitle = new Title(29, "lyon estates", "yellow", 2400);
+var pizzaHutTitle = new Title(31, "pizza hut", "green", 2600);
+var holomaxTitle = new Title(32, "holomax", "green", 2600);
+var hillDaleTitle = new Title(34, "hill dale", "green", 2800);
+var palaisJustice2015 = new Title(35, "palais de justice 2015", "white", 2500);
+var blastFromPastTitle = new Title(37, "blast from the past", "darkblue", 3500);
+var cafe80Title = new Title(39, "cafe 80's", "darkblue", 5000);
 
 var titleList = [
   hillValleyTitle,
